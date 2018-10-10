cbbb128085054b00125767689d4678d5037edb30
thatnewcarsmell <scalajr@gmail.com>
2018-10-10 15:36:31 -0600

    added the ability to choose between blue and red palette colors

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
 document.addEventListener('DOMContentLoaded',function(){
     var canvas = document.querySelector('.canvas');
+    var palette = document.querySelector('.palette');
+    var color = '';
 
-    var redMe = function(){
-        event.target.classList.toggle('clicked-sucka');
-    };
+    var paintIt = function(){
+        let target = event.target.classList;
+        let currentColor = target.item(0);
+        if(currentColor === color){
+            target.remove(currentColor);
+        }
+        else if(target.item(0) !== color){
+            target.remove(currentColor);
+            target.add(color)
+        }
+        return;
+    }
 
-    canvas.addEventListener('click',redMe);
+    var paintSelect = function(){
+        color = event.target.classList.item(0);
+        console.log(color)
+        return;
+    }
+
+    canvas.addEventListener('click',paintIt);
+    palette.addEventListener('click',paintSelect)
 
 });
