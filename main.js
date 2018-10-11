47b5fe735550344386b2bb007732e257d3ecff77
thatnewcarsmell <scalajr@gmail.com>
2018-10-11 14:23:31 -0600

    Expanded palette, cleaned up CSS, added animation to indicate selected color, added paintbrush functionality

--- main.js
+++ main.js
@@ -1,28 +1,58 @@
 document.addEventListener('DOMContentLoaded',function(){
-    var canvas = document.querySelector('.canvas');
-    var palette = document.querySelector('.palette');
     var color = '';
+    var toggler = false;
+    var canvas = document.querySelector('#canvas');
+    var palette = document.querySelector('#palette');
 
-    var paintIt = function(){
+    palette.addEventListener('mousedown',paintSelect);
+    canvas.addEventListener('mouseover',paintIt)
+
+    document.body.onmousedown = function(){
+        toggler = true;
+    }
+
+    document.body.onmouseup = function(){
+        toggler = false;
+    }
+
+    function paintIt(){
         let target = event.target.classList;
         let currentColor = target.item(0);
-        if(currentColor === color){
-            target.remove(currentColor);
-        }
-        else if(target.item(0) !== color){
-            target.remove(currentColor);
-            target.add(color)
+        if(toggler === true){
+            if(color !== ''){
+                if(currentColor !== color){
+                    target.remove(currentColor);
+                    target.add(color);
+                }
+            }
         }
         return;
     }
 
-    var paintSelect = function(){
+    function paintSelect($selectedColor){
         color = event.target.classList.item(0);
+        selectedColor();
         console.log(color)
         return;
     }
 
-    canvas.addEventListener('click',paintIt);
-    palette.addEventListener('click',paintSelect)
+    function selectedColor(){
+        let checkIt = Array.from(palette.children);
+        let target = event.target.classList;
+        console.log(checkIt)
+        if(target.item(1) !== 'selected'){
+            for(let i = 0; i < checkIt.length; i++){
+                checkIt[i].classList.remove('selected');
+            }
+            target.add('selected');
+            return;
+        }
+        else if(target.contains('selected')){
+            target.remove('selected');
+            color = '';
+            return;
+        }
+    }
 
+    console.log('ready!')
 });
